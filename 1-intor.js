4c80909e93229c3112631c1bf12d7216d4a52086
DaveAbraham <Lookinto1@Outlook.com>
2022-06-02 17:38:26 +0500

    first commit

---
+++ 1-intor.js
@@ -0,0 +1,10 @@
+const ammount = 12 
+if(ammount<10){
+    console.log("Small number")
+}else{
+    console.log("Larg naumber")
+}
+
+console.log("First program with node !!!")
+
+
---
+++ 2-globals.js
@@ -0,0 +1,19 @@
+//Globals(anywhere in application we can access them) - No WINDOW!!!(by WINDOW object we have qurey-selector or we have built-in fetch and gist )
+
+
+// __dirname  - path to the current directory
+// __filename - file name
+// require    - function to use modules (CommonJS)
+// module     - info about current module (file)
+//process     - info about env where the program is being executed
+
+
+// console.log(__dirname)
+// console.log(__filename)
+// console.log(require)
+// console.log(module)
+// console.log(process)
+
+setInterval(()=>{
+    console.log("hello world")
+},1000)
